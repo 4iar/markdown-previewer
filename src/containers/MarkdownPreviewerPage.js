4d95dfa440478998a0c095d6f524f1c29c229cbc
4iar <4iar@users.noreply.github.com>
2016-07-06 16:54:18 +0100

    Implement live markdown preview

--- src/containers/MarkdownPreviewerPage.js
+++ src/containers/MarkdownPreviewerPage.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import defaultTextAreaPlaceholder from '../utils/defaultTextAreaPlaceholder';
+import ReactMarkdown from 'react-markdown';
+
+import '../styles/markdown-previewer-page.scss';
 
 
 export default class MarkdownPreviewerPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: defaultTextAreaPlaceholder };
+  }
+
+  onChange(e) {
+    this.setState({ inputValue: e.target.value });
+  }
+
   render() {
     return(
-      <p>Hello World!</p>
+      <div className="container-fluid">
+        <div className="row">
+
+          <div id="input-area" className="col-md-6">
+            <textarea onChange={this.onChange.bind(this)} className="form-control" rows="40" defaultValue={defaultTextAreaPlaceholder}>
+            </textarea>
+          </div>
+
+          <div id="output-area" className="col-md-6">
+            <ReactMarkdown source={this.state.inputValue} />
+          </div>
+
+          <div className="col-md-2"></div>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/utils/defaultTextAreaPlaceholder.js
@@ -0,0 +1,30 @@
+
+const defaultTextAreaPlaceholder = '' +
+  'Heading\n' +
+  '=======\n' +
+  'Sub-heading\n' +
+  '-----------\n' +
+  '### Another deeper heading\n' +
+  'Paragraphs are separated\n' +
+  'by a blank line.\n' +
+  'Leave 2 spaces at the end of a line to do a  \n' +
+  'line break\n' +
+  'Text attributes *italic*, **bold**, \n' +
+  '`monospace`, ~~strikethrough~~ .\n' +
+  '\n' + 
+  'Shopping list:\n' +
+  '* apples\n' +
+  '* oranges\n' +
+  '* pears\n' +
+  '\n' + 
+  'Numbered list:\n' +
+  '1. apples\n' +
+  '2. oranges\n' +
+  '3. pears\n' +
+  'The rain---not the reign---in\n' +
+  'Spain.\n' +
+  ' *[Herman Fassett](https://freecodecamp.com/hermanfassett)*\n'
+
+
+
+export default defaultTextAreaPlaceholder;
